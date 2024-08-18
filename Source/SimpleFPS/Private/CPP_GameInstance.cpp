7c13decb1dab111043d20e6599c25cc91c1d821b
LeeYi-user <khes60122@gmail.com>
2024-08-18 14:54:28 +0800

    setup online subsystem

---
+++ Source/SimpleFPS/Private/CPP_GameInstance.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_GameInstance.h"
+
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+
+UCPP_GameInstance::UCPP_GameInstance()
+{
+
+}
+
+void UCPP_GameInstance::Init()
+{
+
+}
